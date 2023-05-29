9cd0adb21ccae5bb84ab1427601bac0f3357a726
samin <samin@student.42seoul.kr>
2023-05-29 23:16:04 +0900

    fix 상품 교환 서비스 고객 초기 소지금 10000원으로 수정

--- src/project02/Customer.java
+++ src/project02/Customer.java
@@ -1,12 +1,27 @@
 package project02;
 
+import java.util.LinkedList;
+
 public class Customer {
     private int wallet;
+    private LinkedList<Product> productTypeA;
+    private LinkedList<Product> productTypeB;
 
     public Customer() {
-        wallet = 100;
+        wallet = 10000;
+        productTypeA = new LinkedList<>();
+        productTypeB = new LinkedList<>();
     }
 
+
+    public boolean insertCoin(){
+        if (wallet < 100){
+            return false;
+        }
+        wallet -= 100;
+        return true;
+
+    }
     public int getWallet() {
         return wallet;
     }
@@ -15,13 +30,19 @@ public class Customer {
         this.wallet = wallet;
     }
 
-    public boolean insertCoin(){
-        if (wallet >= 100){
-            wallet -= 100;
-            return true;
-        }
-        else {
-            return false;
-        }
+    public LinkedList<Product> getProductTypeA() {
+        return productTypeA;
+    }
+
+    public void setProductTypeA(LinkedList<Product> productTypeA) {
+        this.productTypeA = productTypeA;
+    }
+
+    public LinkedList<Product> getProductTypeB() {
+        return productTypeB;
+    }
+
+    public void setProductTypeB(LinkedList<Product> productTypeB) {
+        this.productTypeB = productTypeB;
     }
 }
