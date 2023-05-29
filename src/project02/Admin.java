244e42c16dc54b7c62450c94f0bf9028fe97a0e2
samin <samin@student.42seoul.kr>
2023-05-29 23:19:24 +0900

    fix 상품 교환 서비스 오프닝 메세지 순서 변경

--- src/project02/Admin.java
+++ src/project02/Admin.java
@@ -1,7 +1,5 @@
 package project02;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
@@ -9,50 +7,65 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Admin {
-    LinkedList<Product> products;
+    private LinkedList<Product> products;
 
     public Admin() {
         products = new LinkedList<>();
-    }
-    public LinkedList<Product> getProducts() {
-        return products;
     }
 
     public void inputProductCodes() {
         Scanner scanner = new Scanner(System.in);
 
+        greeting();
         while (true) {
             String line = scanner.nextLine();
             if (line.equals("END")) {
-                System.out.println("읽기 종료");
-                break;
+                if (products.size() >= 2) {
+                    break;
+                }
+                System.out.println("A, B 등급의 상품은 최소 2종류 이상 준비해야 합니다.");
+                continue;
             }
 
             String[] token = line.split(", ");
 
-            // 형식 검사
             if (token.length != 3) {
-                System.out.println("형식이 잘못 되었습니다");
+                System.out.println("!!형식이 잘못 되었습니다!!");
+                help();
                 continue;
             }
-            // 등급 검사
+
             if (token[1].equals("A") == false &&
                     token[1].equals("B") == false) {
                 System.out.println("잘못된 등급이 입력되었습니다. A 또는 B 등금만 입력해주세요.");
+                help();
                 continue;
             }
-            // 날짜 형식 검증
+
             if (isValidDateFormat(token[2]) == false) {
-                System.out.println("잘못된 유통기한 포멧이 입력되었습니다 " + token[2]);
+                System.out.println("유통기한 형식이 잘못되었습니다 입력되었습니다 " + token[2]);
+                help();
                 continue;
             }
             Product product = new Product(token[0], token[1], token[2]);
             products.add(product);
         }
     }
+    private void greeting() {
+        System.out.println("[빠칭코 상풍등록 시스템]");
+        System.out.println();
+        help();
+        System.out.println("상품정보를 입력해주세요");
+    }
+
+    private void help() {
+        System.out.println("[상품정보 형식 도움말]");
+        System.out.println("상풍명, 등급(A 또는 B), 유통기한");
+        System.out.println("CHICKEN, A, 2024-03-23T02:20:19+09:00");
+        System.out.println();
+    }
 
     private boolean isValidDateFormat(String input) {
-        System.out.println(input);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
 
         try {
@@ -62,4 +75,9 @@ public class Admin {
             return false;
         }
     }
+
+    public LinkedList<Product> getProducts() {
+        return products;
+    }
+
 }
